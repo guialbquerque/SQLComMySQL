8568367155d2ff35034340df403cb5fa924ce715
Guilherme Bispo <guialbquerque@gmail.com>
2023-06-29 20:15:31 -0300

    Checando tabelas

---
+++ 4-ProceduresSQL/TestandoAmbiente2.sql
@@ -0,0 +1,13 @@
+use sucos_vendas;
+
+select * from ITENS_NOTAS_FISCAIS;
+
+select * from NOTAS_FISCAIS;
+
+select * from TABELA_DE_CLIENTES;
+
+select * from TABELA_DE_PRODUTOS;
+
+/*
+Voltando ao MySQL Workbench, testando ambiente
+*/
